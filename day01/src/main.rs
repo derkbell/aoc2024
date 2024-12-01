bf6106c86e770166fdb757b4676c83f427691e1e
Derk Bell <derk.bell@gmail.com>
2024-12-01 06:57:54 +0100

    Remove unneeded type inference and clone

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -22,32 +22,32 @@ fn main() -> io::Result<()> {
     left.sort();
     right.sort();
 
-    let part_a_result = part_a(left.clone(), right.clone());
+    let part_a_result = part_a(&left, &right);
     println!("Part A: {}", part_a_result);
 
-    let part_b_result = part_b(left, right);
+    let part_b_result = part_b(&left, &right);
     println!("Part B: {}", part_b_result);
 
     Ok(())
 }
 
 // Lists are sorted here, so we just take the difference between pairwise elements and sum them up.
-fn part_a(left: Vec<usize>, right: Vec<usize>) -> usize {
+fn part_a(left: &Vec<usize>, right: &Vec<usize>) -> usize {
     left.iter()
         .zip(right.iter())
         .map(|(&a, &b)| usize::max(a, b) - usize::min(a, b))
-        .sum::<usize>()
+        .sum()
 }
 
 // Not the fastest solution, but it works on what we already have. If there would have been a billion entires we should
 // have used a lookup structure for the right side and precount the values.
-fn part_b(left: Vec<usize>, right: Vec<usize>) -> usize {
+fn part_b(left: &Vec<usize>, right: &Vec<usize>) -> usize {
     left.iter()
         .map(|a| {
             let count = right.iter().filter(|&b| a == b).count();
             a * count
         })
-        .sum::<usize>()
+        .sum()
 }
 
 // Extract the two (unsigned) numbers from the line.
